286be9c74b1184d5e1cb3de49ab6695653b69547
stefan <istratie.stefan123@gmail.com>
2025-07-27 11:11:46 +0300

    connect port

--- connect.py
+++ connect.py
@@ -743,4 +743,4 @@ async def api_download(path: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=5001, log_level="info")
